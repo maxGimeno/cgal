414bbc717fb20030a2c07126a70eff3bf6c5d7d4
Come Le Breton <come.le-breton@inria.fr>
2017-07-19 10:53:07 +0200

    Creates a special c3t3 item for CAD mesh domain

---
+++ Polyhedron/demo/Polyhedron/Plugins/Cad/Scene_c3t3_cad_item_config.h
@@ -0,0 +1,14 @@
+#ifndef SCENE_C3T3_CAD_ITEM_CONFIG_H
+#define SCENE_C3T3_CAD_ITEM_CONFIG_H
+
+#ifdef scene_c3t3_cad_item_EXPORTS
+#  define mesh_3_demo_scene_c3t3_cad_item_EXPORTS 1
+#endif
+
+#ifdef mesh_3_demo_scene_c3t3_cad_item_EXPORTS
+#  define SCENE_C3T3_CAD_ITEM_EXPORT Q_DECL_EXPORT
+#else
+#  define SCENE_C3T3_CAD_ITEM_EXPORT Q_DECL_IMPORT
+#endif
+
+#endif // SCENE_C3T3_CAD_ITEM_CONFIG_H
