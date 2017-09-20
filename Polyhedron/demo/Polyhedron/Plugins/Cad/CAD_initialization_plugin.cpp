2c8c50ad1abfa3ba5d5f5f111f2d7e03e4908d14
Come Le Breton <come.le-breton@inria.fr>
2017-09-20 12:50:45 +0200

    Add the initialization plugin (to only initialize the tetradralization, and not refine it), add the refine plugin (to refine an existing tetrahedralization)

---
+++ Polyhedron/demo/Polyhedron/Plugins/Cad/CAD_initialization_plugin.cpp
@@ -0,0 +1,178 @@
+#include <CGAL/Three/Polyhedron_demo_plugin_interface.h>
+
+#include "Scene_cad_item.h"
+
+#include "Scene_c3t3_cad_item.h"
+#include "ui_CAD_mesher_protect_initialization_dialog.h"
+
+#include "C3t3_cad_type.h"
+
+#include <dtkCore>
+#include <dtkContinuousGeometry>
+#include <dtkContinuousGeometryUtils>
+#include <dtkBRep>
+#include <dtkBRepReader>
+#include <dtkNurbsSurface>
+#include <dtkClippedNurbsSurface>
+#include <dtkClippedTrim>
+#include <dtkRationalBezierCurve>
+#include <dtkTrim>
+#include <dtkTopoTrim>
+
+#include <QObject>
+#include <QAction>
+#include <QMainWindow>
+#include <QMenu>
+#include <QApplication>
+#include <QtPlugin>
+#include <QInputDialog>
+#include <QStringList>
+
+// declare the CGAL function
+CGAL::Three::Scene_item* cgal_code_cad_protect_intialization(QWidget* parent,
+                                              dtkBRep*,
+                                              const double sizing);
+
+// To avoid verbose function and named parameters call
+using namespace CGAL::parameters;
+
+using namespace CGAL::Three;
+class Polyhedron_demo_CAD_initialization_plugin :
+    public QObject,
+    protected Polyhedron_demo_plugin_interface
+{
+  Q_OBJECT
+  Q_INTERFACES(CGAL::Three::Polyhedron_demo_plugin_interface)
+  Q_PLUGIN_METADATA(IID "com.geometryfactory.PolyhedronDemo.PluginInterface/1.0")
+
+public:
+  void init(QMainWindow* mainWindow, CGAL::Three::Scene_interface* scene_interface, Messages_interface*) {
+    this->scene = scene_interface;
+    this->mw = mainWindow;
+    actionProtectInitialization = new QAction(tr("Initialization by protection"), mw);
+    actionRandomInitialization = new QAction(tr("Initialization by random shooting"), mw);
+    actionSamplingInitialization = new QAction(tr("Initialization by sampling the surfaces"), mw);
+    actionProtectInitialization->setProperty("subMenuName", "Mesh initialization");
+    actionRandomInitialization->setProperty("subMenuName", "Mesh initialization");
+    actionSamplingInitialization->setProperty("subMenuName", "Mesh initialization");
+    if(actionProtectInitialization) {
+      connect(actionProtectInitialization, SIGNAL(triggered()),
+              this, SLOT(protectInitialization()));
+    }
+    if(actionRandomInitialization != nullptr) {
+      connect(actionRandomInitialization, SIGNAL(triggered()),
+              this, SLOT(randomInitialization()));
+    }
+    if(actionSamplingInitialization != nullptr) {
+        connect(actionSamplingInitialization, SIGNAL(triggered()),
+                this, SLOT(samplingInitialization()));
+    }
+  }
+
+  bool applicable(QAction*) const {
+    return qobject_cast<Scene_cad_item*>(scene->item(scene->selectionIndices().first()));
+  }
+
+  QList<QAction*> actions() const {
+      return QList<QAction*>() << actionProtectInitialization << actionRandomInitialization << actionSamplingInitialization;
+  }
+public Q_SLOTS:
+    void protectInitialization();
+    void randomInitialization();
+    void samplingInitialization();
+
+private:
+    QAction *actionProtectInitialization;
+    QAction *actionRandomInitialization;
+    QAction *actionSamplingInitialization;
+    Scene_interface *scene;
+    QMainWindow *mw;
+}; // end class Polyhedron_demo_remeshing_plugin
+
+void Polyhedron_demo_CAD_initialization_plugin::protectInitialization()
+{
+    std::cerr << "Passes : " << Q_FUNC_INFO << std::endl;
+  Scene_cad_item* cad_item = qobject_cast<Scene_cad_item*>(scene->item(scene->selectionIndices().first()));
+  if(!cad_item)
+    return;
+
+  dtkBRep* brep = cad_item->brep();
+  if(!brep) return;
+
+  QDialog dialog(mw);
+  Ui::CADMesherProtectInitializationDialog ui;
+  ui.setupUi(&dialog);
+  connect(ui.buttonBox, SIGNAL(accepted()),
+          &dialog, SLOT(accept()));
+  connect(ui.buttonBox, SIGNAL(rejected()),
+          &dialog, SLOT(reject()));
+  double diag = scene->len_diagonal();
+
+  ui.sizeSpinBox->setDecimals(4);
+  ui.sizeSpinBox->setRange(diag * 10e-6, // min
+                           diag); // max
+  ui.sizeSpinBox->setValue(diag * 0.05); // default value
+
+  int i = dialog.exec();
+  if(i == QDialog::Rejected)
+    return;
+
+  const double edge_sizing = ui.sizeSpinBox->value();
+  Mesh_domain_with_features* cgal_brep_mesh_domain_with_features = new Mesh_domain_with_features(*brep);
+  ///////////////////////////////////////////////////////////////////
+  //    Recovers the features
+  ///////////////////////////////////////////////////////////////////
+  const std::vector< dtkNurbsSurface *>& nurbs_surfaces = brep->nurbsSurfaces();
+  std::vector< dtkClippedNurbsSurface* > clipped_surfaces;
+  for (auto surf = nurbs_surfaces.begin(); surf != nurbs_surfaces.end(); ++surf) {
+      clipped_surfaces.push_back(new dtkClippedNurbsSurface(*(*surf)));
+  }
+
+  std::list< std::tuple< dtkNurbsCurve *, dtkNurbsCurve *, dtkNurbsCurve * > > features;
+  std::map< const dtkTopoTrim *, dtkNurbsCurve * > topo_trims;
+
+  // ///////////////////////////////////////////////////////////////////
+  // Iterates on all the trims, check if the topo_trim has been found, if it has, add the three curves as a tuple
+  // Else add the topo trim and the first trim found attached to it
+  // As the BRep model is a closed polysurface, for each topo trim there should be two trims associated to it
+  // ///////////////////////////////////////////////////////////////////
+  for (auto it = clipped_surfaces.begin(); it != clipped_surfaces.end(); ++it) {
+      for (auto clip_trim = (*it)->m_clipped_trims.begin(); clip_trim != (*it)->m_clipped_trims.end(); ++clip_trim) {
+          if((*clip_trim)->m_trim.topoTrim()->m_nurbs_curve_3d != nullptr) {
+              auto topo_trim = topo_trims.find((*clip_trim)->m_trim.topoTrim());
+              if (topo_trim == topo_trims.end()) {
+                  topo_trims.insert(std::make_pair((*clip_trim)->m_trim.topoTrim(), (*clip_trim)->m_nurbs_curve));
+              } else {
+                  features.push_back(std::make_tuple(topo_trim->second, topo_trim->first->m_nurbs_curve_3d, (*clip_trim)->m_nurbs_curve));
+              }
+          }
+      }
+  }
+
+  cgal_brep_mesh_domain_with_features->add_features(features.begin(), features.end());
+  std::cerr << "mesh domain adress in intialize : " << cgal_brep_mesh_domain_with_features << std::endl;
+  Mesh_criteria p_criteria( edge_size = edge_sizing);
+
+  C3t3 c3t3;
+  CGAL::internal::Mesh_3::init_c3t3_with_features(c3t3, *cgal_brep_mesh_domain_with_features, p_criteria, false);
+
+  // //Output
+  Scene_c3t3_cad_item *c3t3_cad_item = new Scene_c3t3_cad_item(c3t3, *cgal_brep_mesh_domain_with_features);
+  if(!c3t3_cad_item)
+  {
+    qDebug()<<"c3t3 CAD item not created";
+    return;
+  }
+  c3t3_cad_item->setName(QString("%1 (c3t3)").arg(cad_item->name()));
+  scene->addItem(c3t3_cad_item);
+}
+
+void Polyhedron_demo_CAD_initialization_plugin::randomInitialization(){
+
+}
+
+void Polyhedron_demo_CAD_initialization_plugin::samplingInitialization(){
+
+}
+
+#include "CAD_initialization_plugin.moc"
---
+++ Polyhedron/demo/Polyhedron/Plugins/Cad/CAD_refine_plugin.cpp
@@ -0,0 +1,129 @@
+#include <CGAL/Three/Polyhedron_demo_plugin_interface.h>
+
+#include "Scene_cad_item.h"
+
+#include "Scene_c3t3_cad_item.h"
+#include "ui_CAD_refine_dialog.h"
+
+#include "C3t3_cad_type.h"
+
+#include <dtkCore>
+#include <dtkContinuousGeometry>
+#include <dtkContinuousGeometryUtils>
+#include <dtkBRep>
+#include <dtkBRepReader>
+#include <dtkNurbsSurface>
+#include <dtkClippedNurbsSurface>
+#include <dtkClippedTrim>
+#include <dtkRationalBezierCurve>
+#include <dtkTrim>
+#include <dtkTopoTrim>
+
+#include <QObject>
+#include <QAction>
+#include <QMainWindow>
+#include <QMenu>
+#include <QApplication>
+#include <QtPlugin>
+#include <QInputDialog>
+#include <QStringList>
+
+// declare the CGAL function
+CGAL::Three::Scene_item* cgal_code_cad_refine(QWidget* parent,
+                                              dtkBRep*,
+                                              const double sizing);
+
+// To avoid verbose function and named parameters call
+using namespace CGAL::parameters;
+
+using namespace CGAL::Three;
+class Polyhedron_demo_CAD_refine_plugin :
+    public QObject,
+    protected Polyhedron_demo_plugin_interface
+{
+  Q_OBJECT
+  Q_INTERFACES(CGAL::Three::Polyhedron_demo_plugin_interface)
+  Q_PLUGIN_METADATA(IID "com.geometryfactory.PolyhedronDemo.PluginInterface/1.0")
+
+public:
+  void init(QMainWindow* mainWindow, CGAL::Three::Scene_interface* scene_interface, Messages_interface*) {
+    this->scene = scene_interface;
+    this->mw = mainWindow;
+    actionRefine = new QAction(tr("Refine"), mw);
+    if(actionRefine != nullptr) {
+        connect(actionRefine, SIGNAL(triggered()),
+                this, SLOT(refine()));
+    }
+  }
+
+  bool applicable(QAction*) const {
+    return qobject_cast<Scene_c3t3_cad_item*>(scene->item(scene->selectionIndices().first()));
+  }
+
+  QList<QAction*> actions() const {
+      return QList<QAction*>() << actionRefine;
+  }
+public Q_SLOTS:
+    void refine();
+
+private:
+    QAction *actionRefine;
+    Scene_interface *scene;
+    QMainWindow *mw;
+}; // end class Polyhedron_demo_remeshing_plugin
+
+void Polyhedron_demo_CAD_refine_plugin::refine()
+{
+  Scene_c3t3_cad_item* c3t3_cad_item = qobject_cast<Scene_c3t3_cad_item*>(scene->item(scene->selectionIndices().first()));
+  if(!c3t3_cad_item)
+    return;
+  C3t3& c3t3 = c3t3_cad_item->c3t3();
+  const Mesh_domain_with_features& mesh_domain = c3t3_cad_item->meshDomain();
+  std::cerr << "mesh domain adress in refine : " << &mesh_domain << std::endl;
+  CGAL::Bbox_3 bbox = mesh_domain.bbox();
+  std::cerr << bbox.xmin() << std::endl;
+  QDialog dialog(mw);
+  Ui::CADRefineDialog ui;
+  ui.setupUi(&dialog);
+  connect(ui.buttonBox, SIGNAL(accepted()),
+          &dialog, SLOT(accept()));
+  connect(ui.buttonBox, SIGNAL(rejected()),
+          &dialog, SLOT(reject()));
+  double diag = scene->len_diagonal();
+
+  ui.sizeSpinBox->setDecimals(4);
+  ui.sizeSpinBox->setRange(diag * 10e-6, // min
+                           diag); // max
+  ui.sizeSpinBox->setValue(diag * 0.05); // default value
+
+  ui.angleSpinBox->setDecimals(4);
+  ui.angleSpinBox->setRange(diag * 10e-6, // min
+                           diag); // max
+  ui.angleSpinBox->setValue(diag * 0.05); // default value
+
+  int i = dialog.exec();
+  if(i == QDialog::Rejected)
+    return;
+
+  const double cell_sizing = ui.sizeSpinBox->value();
+  const double angle = ui.angleSpinBox->value();
+
+  Mesh_criteria p_criteria( cell_size = 3.,
+                            facet_angle = 20);
+
+  refine_mesh_3_impl(c3t3, mesh_domain, p_criteria, no_exude(), no_perturb(), no_odt(), no_lloyd(), false);
+
+  c3t3_cad_item->c3t3_changed();
+  std::cerr << c3t3.number_of_cells() << std::endl;
+  // // //Output
+  // Scene_c3t3_cad_item *c3t3_cad_item = new Scene_c3t3_cad_item(c3t3, cgal_brep_mesh_domain_with_features);
+  // if(!c3t3_cad_item)
+  // {
+  //   qDebug()<<"c3t3 CAD item not created";
+  //   return;
+  // }
+  // c3t3_cad_item->setName(QString("%1 (c3t3)").arg(cad_item->name()));
+  // scene->addItem(c3t3_cad_item);
+}
+
+#include "CAD_refine_plugin.moc"
