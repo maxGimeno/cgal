0cd0f1569a169de5da4ad64f010ebbbc721ec57f
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-01-26 12:49:27 +0100

    fixes in includes logic.

--- Point_set_shape_detection_3/include/CGAL/Shape_detection_3/Region_growing.h
+++ Point_set_shape_detection_3/include/CGAL/Shape_detection_3/Region_growing.h
@@ -36,6 +36,8 @@
 
 #include <boost/iterator/counting_iterator.hpp>
 #include <boost/iterator/transform_iterator.hpp>
+#include <boost/iterator/filter_iterator.hpp>
+#include <boost/make_shared.hpp>
 
 namespace CGAL {
 namespace Shape_detection_3 {
