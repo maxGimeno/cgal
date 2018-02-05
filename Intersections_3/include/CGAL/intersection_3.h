2fdcb4b936e23be67fa2e2a3375f71a042bb520d
Andreas Fabri <andreas.fabri@geometryfactory.com>
2018-02-05 15:46:23 +0000

    Add the remaining missing functions

--- Intersections_3/include/CGAL/intersection_3.h
+++ Intersections_3/include/CGAL/intersection_3.h
@@ -62,5 +62,7 @@
 #include <CGAL/internal/Intersections_3/Iso_cuboid_3_Ray_3_do_intersect.h>
 #include <CGAL/internal/Intersections_3/Iso_cuboid_3_Sphere_3_do_intersect.h>
 #include <CGAL/internal/Intersections_3/Iso_cuboid_3_Triangle_3_do_intersect.h>
+#include <CGAL/internal/Intersections_3/Tetrahedron_3_do_intersect.h>
+#include <CGAL/internal/Intersections_3/Triangle_3_Sphere_3_do_intersect.h>
 
 #endif // CGAL_INTERSECTION_3_H
