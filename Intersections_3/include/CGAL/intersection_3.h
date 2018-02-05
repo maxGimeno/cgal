df17e6cb829b6814c7191081a4da15a727f97f0c
Andreas Fabri <andreas.fabri@geometryfactory.com>
2018-02-05 11:32:13 +0000

    Add Iso_cuboid / Sphere

--- Intersections_3/include/CGAL/intersection_3.h
+++ Intersections_3/include/CGAL/intersection_3.h
@@ -60,5 +60,6 @@
 
 #include <CGAL/internal/Intersections_3/Iso_cuboid_3_Segment_3_do_intersect.h>
 #include <CGAL/internal/Intersections_3/Iso_cuboid_3_Ray_3_do_intersect.h>
+#include <CGAL/internal/Intersections_3/Iso_cuboid_3_Sphere_3_do_intersect.h>
 
 #endif // CGAL_INTERSECTION_3_H
